45b05da555ebdbc21eee1b95f86835ab117d83c5
Noan Jard <noan.pujol@gmail.com>
2026-02-05 16:19:28 +0100

    challenging tab added 2

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/binarytree_visualizer/"
+  base: "/binarytree_playground/"
 })
